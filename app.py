7145650a322b7c70a1f9b9ec95313f3a59512782
Antonio Hannesch Junior <hanneschjr@gmail.com>
2025-05-27 21:42:03 -0300

    codigos até a aula Index

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 # Arquivo de inicialização do Software de Gestão de Escritório de Advocacia - Azimov
+import dash
+import dash_bootstrap_components as dbc
 
+estilos = ['https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/front-awesome.min.css', dbc.themes.LUX] # onde pega os ícones e o tema LUX
+dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css" # baixa os bootstrap templates que tem ligação com o LUX (só o css)
 
+app = dash.Dash(__name__, external_stylesheets=estilos + [dbc_css]) # a folha de estilos é o front-awesome + dbc.theme + css
 
+app.config['suppress_callback_exceptions'] = True # suprime o lançamentos de exceções que alguns callbacks podem conter
+app.scripts.config.serve_locally = True # informa oa Dash que ele deve servir seus arquivos JavaScript internamente, a partir do próprio servidor Flask (localmente)
+server = app.server # app é o app Desh, app.server é o app Flask interno e server = app.server torna disponível fora do Dash
 
---
+++ index.py
@@ -0,0 +1,70 @@
+import dash
+from dash import html, dcc, Input, Output, State
+import dash_bootstrap_components as dbc
+import pandas as pd
+
+# import from folders
+from app import *
+from components import home, sidebar
+
+# Criar estrutura para Sotre intermediária ===============
+
+
+
+
+# ====================== Layout =================== #
+app.layout = dbc.Container([
+    # Store e Location
+    dcc.Location(id='url'),
+    dcc.Store(id='store_intermedio', data={}),
+    dcc.Store(id='store_adv', data={}),
+    dcc.Store(id='store_proc', data={}),
+    html.Div(id='dif_fantasma', 'children'),
+
+    # Layout
+    dbc.Row([
+        dbc.Col([
+            sidebar.layout
+        ], md=2, style={'padding': '0px'}),
+        dbc.Col([
+            dbc.Container(id='page-content', fluid=True, style={'height': '100%', 'width': '100%', 'padding-left': '14px'})
+        ], md=10, style={'padding': '0px'})
+
+
+    ])
+
+], fluid=True)
+
+# ====================== Callbacks =================== #
+# URL callback to update page content
+@app.callback(Output('page-content', 'children'), Input('url', 'pathname'))
+def render_page(pathname):
+    if pathname == '/home' or pathname == '/':
+        return home.layout
+    else:
+        return dbc.Container([
+            html.H1('404: Not found', className='text-danger'),
+            html.Hr(),
+            html.P(f"O caminho '{pathname}' não foi reconhecido..."),
+            html.P('Use a NavBar para retornar ao sistema de maneira correta.')
+        ])
+
+# Dcc.Store back to file
+@app.callback(
+    Output('div_fantasma', 'children'), 
+    Input('store_adv', 'data'),
+    Input('store_proc', 'data'),
+)
+def update_file(adv_data, proc_data):
+    df_adv_aux = pd.DataFrame(adv_data)
+    df_proc_aux = pd.DataFrame(proc_data)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+
+
+
