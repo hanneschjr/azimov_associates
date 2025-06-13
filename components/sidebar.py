ab0cdf36c97c5d74e6f7579a966e69da15dafe68
Antonio Hannesch Junior <hannesch@PAT-22145.cds.local>
2025-06-13 12:07:51 -0300

    Fim da estrutura do projeto - início da sidebar

--- components/sidebar.py
+++ components/sidebar.py
@@ -1,5 +1,4 @@
 import dash
-import plotly.express as px
 from dash import html, dcc, callback_context
 from dash.dependencies import Input, Output, State, ALL
 import dash_bootstrap_components as dbc
@@ -11,4 +10,22 @@ from components import modal_advogados, modal_novo_advogado, modal_processo
 from app import app
 
 # =========== Layout ========== #
-layout = dbc.Container()
+layout = dbc.Container([
+    modal_novo_advogado.layout,
+    modal_processo.layout,
+    modal_advogados.layout,
+    dbc.Container([
+        
+    ])
+])
+
+
+
+
+# ========= Callbacks =========== #
+# Abrir Model New Lawyer
+
+
+
+
+# Abrir Modal Lawyers
