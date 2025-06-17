b74f9b86f3c480ec86cbb2af27d36877fe2a5c78
Antonio Hannesch Junior <hannesch@PAT-22145.cds.local>
2025-06-17 12:54:15 -0300

    correções finais

--- components/modal_novo_advogado.py
+++ components/modal_novo_advogado.py
@@ -4,11 +4,9 @@ from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import pandas as pd
 
-# from dash import dash_table
-# from dash.dash_table.Format import Group
 
 from app import app
-# from components import home
+
 
 # ============= Layout ============== #
 layout = dbc.Modal([
